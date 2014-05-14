19452aa84ce17daadd8f0e6897aa4ab41c7b6d4b
Chris Morgan <christopher.j.morgan@gmail.com>
2014-05-14 15:56:58 -0700

    Switch inventory.html to use ui.bootstrap modals

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/* Controllers */
-
 angular.module('beerbarter.controllers', []).
   controller('HomeCtrl', ['$scope', '$http',
     function($scope, $http) {
@@ -18,11 +16,42 @@ angular.module('beerbarter.controllers', []).
   .controller('OffersCtrl', [function() {
 
   }])
-  .controller('InventoryCtrl', ['$scope', '$http', 
-    function($scope, $http) {
+  .controller('AddBeerToInventoryModalCtrl', ['$scope', '$modalInstance',
+    function($scope, $modalInstance) {
+      $scope.add = function() {
+        // TODO: actually add the beer
+        $modalInstance.close('add');
+      };
+      $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+      };
+  }])
+  .controller('ShareInventoryModalCtrl', ['$scope', '$modalInstance',
+    function($scope, $modalInstance) {
+      $scope.done = function() {
+        $modalInstance.close('done');
+      };
+  }])
+  .controller('InventoryCtrl', ['$scope', '$http', '$modal',
+    function($scope, $http, $modal) {
       $http.get('testdata/inventory.json').success(function(data) {
         $scope.beers = data;
       });
+
+      $scope.shareInventory = function() {
+        var modalInstance = $modal.open({
+          templateUrl: 'partials/shareInventoryModal.html',
+          controller: 'ShareInventoryModalCtrl',
+          // TODO: how are we sharing, via URL or some other way?
+        });
+      }
+      
+      $scope.addBeer = function() {
+        var modalInstance = $modal.open({
+          templateUrl: 'partials/addBeerToInventoryModal.html',
+          controller: 'AddBeerToInventoryModalCtrl',
+        });
+      }
   }])
   .controller('WantlistCtrl', ['$scope', '$http', 
     function($scope, $http) {
