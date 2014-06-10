02f21eabb9c0151f073259272a1efdfb7aa92052
Chris Morgan <christopher.j.morgan@gmail.com>
2014-06-09 22:38:48 -0700

    Add GA pageview tracking in controllers.js via angular-ga.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,20 +1,23 @@
 'use strict';
 
 angular.module('beerbarter.controllers', []).
-  controller('HomeCtrl', ['$scope', '$http',
-    function($scope, $http) {
+  controller('HomeCtrl', ['$scope', '$http', 'ga',
+    function($scope, $http, ga) {
+      ga('send', 'pageview', {title: 'Home'});
       $http.get('testdata/announcements.json').success(function(data) {
         $scope.announcements = data;
       });
   }])
-  .controller('TradingCtrl', ['$scope', '$http', 
-    function($scope, $http) {
+  .controller('TradingCtrl', ['$scope', '$http', 'ga',
+    function($scope, $http, ga) {
+      ga('send', 'pageview', {title: 'Trading'});
       $http.get('testdata/matches.json').success(function(data) {
         $scope.matches = data;
       });
   }])
-  .controller('OffersCtrl', ['$scope', '$http', 
-    function($scope, $http) {
+  .controller('OffersCtrl', ['$scope', '$http', 'ga',
+    function($scope, $http, ga) {
+      ga('send', 'pageview', {title: 'Offers'});
       $http.get('testdata/offers.json').success(function(data) {
         $scope.offers = data;
       });
@@ -22,21 +25,25 @@ angular.module('beerbarter.controllers', []).
   .controller('AddBeerToInventoryModalCtrl', ['$scope', '$modalInstance',
     function($scope, $modalInstance) {
       $scope.add = function() {
+        // TODO: Track add beer event via GA
         // TODO: actually add the beer
         $modalInstance.close('add');
       };
       $scope.cancel = function() {
+        // TODO: Track cancel add beer event via GA
         $modalInstance.dismiss('cancel');
       };
   }])
   .controller('ShareInventoryModalCtrl', ['$scope', '$modalInstance',
     function($scope, $modalInstance) {
+      // TODO: Track sharing
       $scope.done = function() {
         $modalInstance.close('done');
       };
   }])
-  .controller('InventoryCtrl', ['$scope', '$http', '$modal',
-    function($scope, $http, $modal) {
+  .controller('InventoryCtrl', ['$scope', '$http', '$modal', 'ga',
+    function($scope, $http, $modal, ga) {
+      ga('send', 'pageview', {title: 'Inventory'});
       $http.get('testdata/inventory.json').success(function(data) {
         $scope.beers = data;
       });
@@ -59,10 +66,12 @@ angular.module('beerbarter.controllers', []).
   .controller('AddBeerToWantlistModalCtrl', ['$scope', '$modalInstance',
     function($scope, $modalInstance) {
       $scope.add = function() {
+        // TODO: Track the event
         // TODO: actually add the beer
         $modalInstance.close('add');
       };
       $scope.cancel = function() {
+        // TODO: Track the event
         $modalInstance.dismiss('cancel');
       };
   }])
@@ -72,8 +81,9 @@ angular.module('beerbarter.controllers', []).
         $modalInstance.close('done');
       };
   }])
-  .controller('WantlistCtrl', ['$scope', '$http', '$modal',
-    function($scope, $http, $modal) {
+  .controller('WantlistCtrl', ['$scope', '$http', '$modal', 'ga',
+    function($scope, $http, $modal, ga) {
+      ga('send', 'pageview', {title: 'Want List'});
       $http.get('testdata/wantlist.json').success(function(data) {
         $scope.beers = data;
       });
